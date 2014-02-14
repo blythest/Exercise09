49aef2faf28a6b33b6d2482be17053c424ef0c94
Blythe Sheldon <blythe.sheldon@gmail.com>
2014-02-14 13:51:55 -0800

    final version of recursion assignment

--- recursion.py
+++ recursion.py
@@ -51,19 +51,24 @@ def reverse(l):
 # l = [1,2,3,4,5]
 # print reverse(l)
 
-# Fibonacci returns the nth fibonacci number. The nth fibonacci number is
-# defined as fib(n) = fib(n-1) + fib(n-2)
 def fibonacci(n):
-    if n <= 3: 
-        return n
-    return fibonacci(n-2) + fibonacci(n-1)
+    if n <= 1:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
 
 # Finds the item i in the list l.... RECURSIVELY
-# def find(l, i):
-#     if len(l) == 1:
-#         return i
-#     else
-#         return find(l,i)
+def find(l, i):
+    if l == []:
+        return None
+    elif l[0] == i:
+        return i
+    rest = l[1:]
+    return find(rest,i)
+
+# n = [1,2,3,4,5,6]
+
+# print find(n,3)
+
 
 # Determines if a string is a palindrome
 def palindrome(some_string):
@@ -79,14 +84,42 @@ def palindrome(some_string):
             return False
         
 
-s = 'arrow'
-print palindrome(s)
+# s = 'arrow'
+# print palindrome(s)
 
-# Given the width and height of a sheet of paper, and the number of times to fold it, return the final dimensions of the sheet as a tuple. Assume that you always fold in half along the longest edge of the sheet.
+# Given the width and height of a sheet of paper, and the number of times to fold it,
+# return the final dimensions of the sheet as a tuple.
+# Assume that you always fold in half along the longest edge of the sheet.
 def fold_paper(width, height, folds):
-    return (0, 0)
+
+    if folds == 0:
+        return (width, height)
+    else:
+
+        # start with 8 x 12 paper: width is 8, height is 12
+        # height is greater. new height is 6, width is 8.
+        # width is greater. height is 6. width is 4.
+        # height is greater. height is 3. width is 4.
+        # width is great. height is 3. width is 2. 
+
+        if width > height:
+            # fold the width
+            return fold_paper(width/2.0, height, folds-1)
+        elif width < height:
+            return fold_paper(width, height/2.0, folds-1)
+
+# print fold_paper(12, 8, 4)
+            
 
 # Count up
 # Print all the numbers from 0 to target
 def count_up(target, n):
-    return
+    if n == target:
+        return n
+    else:
+        print n
+        return count_up(target, n + 1)
+        
+
+count_up(100, 0)
+ 
